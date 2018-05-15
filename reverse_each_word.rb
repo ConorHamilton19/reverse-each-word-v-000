fc8674f1ae4f476e2a6f9ec737697492a11af745
Conor Hamilton <conhamil19@gmail.com>
2018-05-15 03:48:03 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,6 @@ def reverse_each_word(string)
   empty_array = []
   new_array.each do |word|
   word.reverse << empty_array
-  reverse_string = empty_array.join(" ")
+  empty_array.join(" ")
   end
-  return reverse_string(string)
 end
